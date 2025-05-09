ca3325012b88d5210de537b562a9d264f82a7b92
Jan Prindis <jan.prindis3@gmail.com>
2025-05-09 20:53:30 +0200

    Hoří ti strom

--- 12_ForestFire/main.py
+++ 12_ForestFire/main.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.colors import ListedColormap
 
-
 class Neighborhood(Enum):
     VON_NEUMANN = [(1, 0), (-1, 0), (0, 1), (0, -1)]
     MOORE = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
@@ -17,98 +16,121 @@ class Cell(Enum):
     BURNING = 2
     BURNT = 3
 
+class ForestFire:
+    def __init__(self, map_size: tuple[int, int], neighborhood: Neighborhood, title: str = "Forest Fire"):
+        self.color_map = ListedColormap([
+            "#522600",  # EMPTY
+            "#009c00",  # TREE
+            "#ff8000",  # BURNING
+            "#000000"  # BURNT
+        ])
 
-color_map = ListedColormap([
-    "#522600",  # EMPTY
-    "#009c00",  # TREE
-    "#ff8000",  # BURNING
-    "#000000"  # BURNT
-])
+        self.p = 0.05       # Regrowing probability
+        self.f = 0.001      # Self-ignition probability
+        self.density = 0.5  # Initial probability that cell will be a tree upon map generation
 
+        self.neighborhood = neighborhood
+        self.map_matrix = self.__generate_map(map_size)
 
-p = 0.05
-f = 0.001
-density = 0.5
+        # Matplotlib setup
+        self.fig, self.ax = plt.subplots()
+        self.numeric_matrix = np.vectorize(lambda cell: cell.value)(self.map_matrix)
+        self.img = self.ax.imshow(self.numeric_matrix, cmap=self.color_map, vmin=0, vmax=3)
+        self.ax.set_xticks([])
+        self.ax.set_yticks([])
+        self.ax.set_title(title)
 
-
-def generate_map(map_size):
-    map_matrix = np.empty(map_size, dtype=Cell)
-    for i in range(map_size[0]):
-        for j in range(map_size[1]):
-            if random.random() < density:
-                map_matrix[i][j] = Cell.TREE
-            else:
-                map_matrix[i][j] = Cell.EMPTY
-
-    return map_matrix
-
-
-def update(frame):
-    global map_matrix
-    map_matrix = tick(map_matrix, Neighborhood.MOORE)
-
-    numeric_matrix = np.vectorize(lambda cell: cell.value)(map_matrix)
-    img.set_data(numeric_matrix)
-
-    return [img]
-
-
-def tick(map_matrix, neighborhood: Neighborhood):
-    result = np.empty_like(map_matrix)
-    shape = map_matrix.shape
-    for i in range(shape[0]):
-        for j in range(shape[1]):
-            cell = map_matrix[i][j]
-
-            if cell == Cell.EMPTY:
-                if random.random() < p:
-                    result[i][j] = Cell.TREE
+    def __generate_map(self, map_size):
+        matrix = np.empty(map_size, dtype=Cell)
+        for i in range(map_size[0]):
+            for j in range(map_size[1]):
+                if random.random() < self.density:
+                    matrix[i][j] = Cell.TREE
                 else:
-                    result[i][j] = cell
+                    matrix[i][j] = Cell.EMPTY
+        return matrix
 
-            elif cell == Cell.BURNT:
-                result[i][j] = Cell.EMPTY
+    def __tick(self):
+        result = np.empty_like(self.map_matrix)
+        shape = self.map_matrix.shape
+        for i in range(shape[0]):
+            for j in range(shape[1]):
+                cell = self.map_matrix[i][j]
 
-            elif cell == Cell.BURNING:
-                result[i][j] = Cell.BURNT
-
-            elif cell == Cell.TREE:
-                is_neighbor_burning = False
-
-                for offset in neighborhood.value:
-                    new_i = i + offset[0]
-                    new_j = j + offset[1]
-
-                    if new_i >= shape[0] or new_i < 0:
-                        continue
-
-                    if new_j >= shape[1] or new_j < 0:
-                        continue
-
-                    if map_matrix[new_i][new_j] == Cell.BURNING:
-                        is_neighbor_burning = True
-                        break
-
-                if is_neighbor_burning:
-                    result[i][j] = Cell.BURNING
-                else:
-                    if random.random() < f:
-                        result[i][j] = Cell.BURNING
+                # Empty cell
+                if cell == Cell.EMPTY:
+                    # Becomes tree with probability p
+                    if random.random() < self.p:
+                        result[i][j] = Cell.TREE
                     else:
                         result[i][j] = cell
 
-    return result
+                # Burnt cell becomes empty
+                elif cell == Cell.BURNT:
+                    result[i][j] = Cell.EMPTY
+
+                # Burning cell becomes burnt
+                elif cell == Cell.BURNING:
+                    result[i][j] = Cell.BURNT
+
+                # Tree cell
+                elif cell == Cell.TREE:
+                    is_neighbor_burning = False
+
+                    for offset in self.neighborhood.value:
+                        new_i = i + offset[0]
+                        new_j = j + offset[1]
+
+                        if new_i >= shape[0] or new_i < 0:
+                            continue
+
+                        if new_j >= shape[1] or new_j < 0:
+                            continue
+
+                        if self.map_matrix[new_i][new_j] == Cell.BURNING:
+                            is_neighbor_burning = True
+                            break
+
+                    # Catches fire if neighbor is on fire
+                    if is_neighbor_burning:
+                        result[i][j] = Cell.BURNING
+                    else:
+                        # Or catches on fire by itself with probability f
+                        if random.random() < self.f:
+                            result[i][j] = Cell.BURNING
+                        else:
+                            result[i][j] = cell
+
+        return result
+
+    # Animation stuff
+    def __update_animation_frame(self, frame):
+        self.map_matrix = self.__tick()
+        self.numeric_matrix = np.vectorize(lambda cell: cell.value)(self.map_matrix)
+        self.img.set_data(self.numeric_matrix)
+        return [self.img]
+
+    def run(self, interval=100):
+        animation = FuncAnimation(self.fig, self.__update_animation_frame, interval=interval, cache_frame_data=False)
+        plt.show()
+
+
+# Helper method for multithreading
+def run_simulation(map_size, neighborhood, title):
+    sim = ForestFire(map_size, neighborhood, title)
+    sim.run()
 
 if __name__ == '__main__':
-    map_matrix = generate_map((100, 100))
+    # Multithreading, so we can run them side by side
+    from multiprocessing import Process
+    from multiprocessing import set_start_method
+    set_start_method('spawn')
 
-    fig, ax = plt.subplots()
-    numeric_matrix = np.vectorize(lambda cell: cell.value)(map_matrix)
+    p1 = Process(target=run_simulation, args=((100, 100), Neighborhood.MOORE, "Moore"))
+    p2 = Process(target=run_simulation, args=((100, 100), Neighborhood.VON_NEUMANN, "Von Neumann"))
 
-    img = ax.imshow(numeric_matrix, cmap=color_map, vmin=0, vmax=3)
-    ax.set_xticks([])
-    ax.set_yticks([])
+    p1.start()
+    p2.start()
 
-
-    animation = FuncAnimation(fig, update, interval=100, cache_frame_data=False)
-    plt.show()
+    p1.join()
+    p2.join()
---
+++ 12_ForestFire/readme.md
@@ -0,0 +1,35 @@
+# TASK 5 - Forest Fire
+
+Cílem úkolu bylo vytvořit buněčný automat, který bude simulovat požár lesu. 
+
+---
+## Postup
+- Vytvoří se matice reprezentující mapu, kde každá buňka bude s pravěpodobností 50 % (`density`) strom, nebo prázdné.
+- Poté se spustí simulace.
+- Každý tick simulace:
+  - Pokud je aktuální buňka _**prázdná**_
+    - S pravděpodobností 5 % (`p`) se přemění na strom, jinak zůstane prázdná
+  - Pokud je aktuální buňka _**ohořelý strom**_
+    - Přemění se na prázdnou buňku
+  - Pokud aktuální buňka _**hoří**_
+    - Přemění se na ohořelou
+  - Pokud je buňka _**strom**_
+    - Pokud hoří jeden ze stromů v sousedství (definováno podle `neighborhood`), tak začně hořet
+    - Pokud žádný ze sousedů nehoří
+      - S pravděpodobností 0.1 % dojde k samovznícení
+      - Jinak zůstává stromem
+
+---
+
+## Sousedství
+- Lze nastavit 2 typy sousedství podle parametru předaného v konstruktoru třídy
+  - _**Von Neumann**_ - Sousedi se počítají pouze nahoře, dole, vlevo a vpravo
+  - _**Moore**_ - Navíc obsahuje diagonální sousedy
+
+## Výstup
+- Po zavolání metody `run` se spustí simulace prostředí.
+- Pro lepší vizualizaci obou voleb sousedů se po spuštění mainu vytvoří separátní instance pro každého z nich.
+
+![](./result.gif)
+
+(Výstup skriptu, [Imgur link na gif](https://imgur.com/a/niE6fGn))
